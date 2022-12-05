594532c2615fd000847134c70fe7a538b186b4ab
andreej21 <108309883+andreej21@users.noreply.github.com>
2022-12-05 01:02:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,74 +50,74 @@ user_pw = driver.find_element(By.XPATH,'//*[@id="pass"]').send_keys(password,Key
 time.sleep(2)
 list_groups = ['https://www.facebook.com/groups/3207403259329762/','https://www.facebook.com/groups/jesusadrianromeromusicacristiana/','https://www.facebook.com/groups/jesusadrianromerooficial/','https://www.facebook.com/groups/JesusAdrianRomeroMusicaCristiana1/','https://www.facebook.com/groups/1175792979441047']
 
-    # try:
+try:
 
-    #     for i in list_groups:
+    for i in list_groups:
 
-    #         driver.get(i)
+        driver.get(i)
 
 
-    #         time.sleep(2)
-    
-    #         for i in range(0,12):
-    #             driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-    #             time.sleep(2)
+        time.sleep(2)
 
-    #         try:
+        for i in range(0,12):
+            driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+            time.sleep(2)
 
-    #             feed = driver.find_element(By.XPATH,'//div[@role="feed"]')
-    #             all_posts = feed.find_elements(By.XPATH,'./div[@class="x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"]')
-    #             time.sleep(1)
-    #         except:
-    #             continue
+        try:
 
-    #         for post in all_posts:
-    #             try:
-    #                 likes = post.find_element(By.XPATH,'.//span[@class="x16hj40l"]').get_attribute('innerText')
+            feed = driver.find_element(By.XPATH,'//div[@role="feed"]')
+            all_posts = feed.find_elements(By.XPATH,'./div[@class="x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"]')
+            time.sleep(1)
+        except:
+            continue
 
-    #             except:
-    #                 pass
-    #             if 'K' in likes:
+        for post in all_posts:
+            try:
+                likes = post.find_element(By.XPATH,'.//span[@class="x16hj40l"]').get_attribute('innerText')
+
+            except:
+                pass
+            if 'K' in likes:
+                
+                try:
+                    try:
+                        post.find_element(By.XPATH,'.//div[@class="x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f" and @role="button"]').click()
+                        time.sleep(1)
+                        text_content = post.find_element(By.XPATH,'.//div[@dir="auto" and @style="text-align: start;"]').get_attribute('innerText')
+                        if "See more" in text_content:
+                            text_content.replace("See more",'')    
+                        text_contents.append(text_content)                        
+                    except:                            
+                        text_content = post.find_element(By.XPATH,'.//div[@dir="auto" and @style="text-align: start;"]').get_attribute('innerText')
+                        if "See more" in text_content:
+                            text_content.replace("See more",'')
+                        text_contents.append(text_content)
+                except:
+                    pass
+                try:
+                    image = post.find_element(By.TAG_NAME,'img').get_attribute("src")
                     
-    #                 try:
-    #                     try:
-    #                         post.find_element(By.XPATH,'.//div[@class="x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f" and @role="button"]').click()
-    #                         time.sleep(1)
-    #                         text_content = post.find_element(By.XPATH,'.//div[@dir="auto" and @style="text-align: start;"]').get_attribute('innerText')
-    #                         if "See more" in text_content:
-    #                             text_content.replace("See more",'')    
-    #                         text_contents.append(text_content)                        
-    #                     except:                            
-    #                         text_content = post.find_element(By.XPATH,'.//div[@dir="auto" and @style="text-align: start;"]').get_attribute('innerText')
-    #                         if "See more" in text_content:
-    #                             text_content.replace("See more",'')
-    #                         text_contents.append(text_content)
-    #                 except:
-    #                     pass
-    #                 try:
-    #                     image = post.find_element(By.TAG_NAME,'img').get_attribute("src")
-                        
-    #                     try:
+                    try:
 
-    #                         img_content = requests.get(image).content
-    #                         img_file = io.BytesIO(img_content)
-    #                         img = Image.open(img_file)
-    #                         file_pth = full_directory+f'\img{num}.jpeg'
-    #                         with open(file_pth,'wb') as file:
-    #                             img.save(file,'JPEG')
-    #                         num+=1
+                        img_content = requests.get(image).content
+                        img_file = io.BytesIO(img_content)
+                        img = Image.open(img_file)
+                        file_pth = full_directory+f'\img{num}.jpeg'
+                        with open(file_pth,'wb') as file:
+                            img.save(file,'JPEG')
+                        num+=1
 
-    #                     except:
-    #                         pass
-    #                 except:
-    #                     pass
+                    except:
+                        pass
+                except:
+                    pass
 
 
-    #         text_data = pd.DataFrame({'Content':text_contents})
-    #         text_data.to_excel('Fb.xlsx',index=False)
-        
-    # except:
-    #     driver.quit()
+        text_data = pd.DataFrame({'Content':text_contents})
+        text_data.to_excel('Fb.xlsx',index=False)
+    
+except:
+    driver.quit()
 group_for_posting = data["post_link"]
 
 dir_list = os.listdir(full_directory)
